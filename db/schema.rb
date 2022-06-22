b0f68316969556eac4f400a101d9cd2f8b23d5d3
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2022-06-21 22:21:03 -0300

    feat: add login page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_172132) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_165135) do
   create_table "friends", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
@@ -19,6 +19,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_172132) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "password_salt"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
